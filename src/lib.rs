8b39db96de5d2dfe670e99885e75701f7590b973
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-06-12 23:12:18 +0300

    WiP

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@
 //!
 //! Not performance-optimized
 
-extern crate http;
-extern crate httparse;
+pub extern crate http;
+pub extern crate httparse;
+extern crate bytes;
 
 pub type Request = http::request::Request<()>;
 pub type Response = http::response::Response<()>;
@@ -21,6 +22,7 @@ pub enum Error {
     HeaderName(http::header::InvalidHeaderName),
     HeaderValue(http::header::InvalidHeaderValue),
     StatusCode(http::status::InvalidStatusCode),
+    InvalidAuthority(http::uri::InvalidUriBytes),
 }
 
 impl std::fmt::Display for Error {
@@ -31,6 +33,7 @@ impl std::fmt::Display for Error {
             Error::HeaderName(x) => x.fmt(f),
             Error::HeaderValue(x) => x.fmt(f),
             Error::StatusCode(x) => x.fmt(f),
+            Error::InvalidAuthority(x) => x.fmt(f),
         }
     }
 }
@@ -42,19 +45,22 @@ impl std::error::Error for Error {
             Error::HeaderName(x) => x,
             Error::HeaderValue(x) => x,
             Error::StatusCode(x) => x,
+            Error::InvalidAuthority(x) => x,
         })
     }
 }
 
 use std::str::FromStr;
-
+use http::uri::{Parts as UriParts,PathAndQuery,Authority};
+use http::{Method,StatusCode};
+use http::header::{HeaderName,HeaderValue,HOST};
 
 /// Parse this byte buffer into a `Request` plus remaining trailing bytes.
 /// Returns `Ok(None)` if not enough bytes yet to produce a complete request.
 /// Allocates a space for 50 headers (about 800 bytes) in stack each time.
 pub fn parse_request_header_easy(buf: &[u8]) -> Result<Option<(Request, &[u8])>, Error> {
     let mut h = [httparse::EMPTY_HEADER; 50];
-    parse_request_header(buf, h.as_mut())
+    parse_request_header(buf, h.as_mut(), None)
 }
 
 /// Parse this byte buffer into a `Response` plus remaining trailing bytes.
@@ -66,10 +72,18 @@ pub fn parse_response_header_easy(buf: &[u8]) -> Result<Option<(Response, &[u8])
 }
 
 /// Parse this byte buffer into a `Request` plus remaining trailing bytes.
+/// 
 /// Returns `Ok(None)` if not enough bytes yet to produce a complete request.
+/// 
+/// If `scheme` is specified then information from `Host:`
+/// header is filled in into URL, if it exists. In case of no
+/// host header the URL would be scheme-less.
+/// 
+/// TODO: also fill in HTTP basic auth data if present in request
 pub fn parse_request_header<'a,'b>(
     buf: &'a[u8],
     headers_buffer: &'b mut [httparse::Header<'a>],
+    scheme: Option<http::uri::Scheme>,
 ) -> Result<Option<(Request, &'a[u8])>, Error> {
     let mut x = httparse::Request::new(headers_buffer);
     let n = match x.parse(buf).map_err(Error::Parse)? {
@@ -79,14 +93,25 @@ pub fn parse_request_header<'a,'b>(
     let trailer = &buf[n..];
     let mut r = Request::new(());
     *r.method_mut() =
-        http::Method::from_str(x.method.unwrap()).map_err(|_| Error::Parse(httparse::Error::Token))?;
+        Method::from_str(x.method.unwrap()).map_err(|_| Error::Parse(httparse::Error::Token))?;
     *r.version_mut() = http::Version::HTTP_11; // FIXME?
-    *r.uri_mut() = http::Uri::from_str(x.path.unwrap()).map_err(Error::Path)?;
+    let mut up : UriParts = Default::default();
+    up.path_and_query = Some(PathAndQuery::from_str(x.path.unwrap()).map_err(Error::Path)?);
+
     for h in x.headers {
-        let n = http::header::HeaderName::from_str(h.name).map_err(Error::HeaderName)?;
-        let v = http::header::HeaderValue::from_bytes(h.value).map_err(Error::HeaderValue)?;
+        let n = HeaderName::from_str(h.name).map_err(Error::HeaderName)?;
+        let v = HeaderValue::from_bytes(h.value).map_err(Error::HeaderValue)?;
         r.headers_mut().append(n, v);
     }
+    if scheme.is_some() {
+        if let Some(h) = r.headers().get(HOST) {
+            up.scheme = scheme;
+            let a = bytes::Bytes::from(h.as_bytes());
+            let a = Authority::from_shared(a).map_err(Error::InvalidAuthority)?;
+            up.authority = Some(a);
+        }
+    }
+    *r.uri_mut() = http::Uri::from_parts(up).unwrap();
     Ok(Some((r, trailer)))
 }
 
@@ -104,12 +129,12 @@ pub fn parse_response_header<'a,'b>(
     };
     let trailer = &buf[n..];
     let mut r = Response::new(());
-    *r.status_mut() = http::StatusCode::from_u16(x.code.unwrap()).map_err(Error::StatusCode)?;
+    *r.status_mut() = StatusCode::from_u16(x.code.unwrap()).map_err(Error::StatusCode)?;
     // x.reason goes to nowhere
     *r.version_mut() = http::Version::HTTP_11; // FIXME?
     for h in x.headers {
-        let n = http::header::HeaderName::from_str(h.name).map_err(Error::HeaderName)?;
-        let v = http::header::HeaderValue::from_bytes(h.value).map_err(Error::HeaderValue)?;
+        let n = HeaderName::from_str(h.name).map_err(Error::HeaderName)?;
+        let v = HeaderValue::from_bytes(h.value).map_err(Error::HeaderValue)?;
         r.headers_mut().append(n, v);
     }
     Ok(Some((r, trailer)))
@@ -132,7 +157,7 @@ pub fn write_request_header<T>(r: &http::Request<T>, mut io: impl std::io::Write
     let verb = r.method().as_str();
     let path = r.uri().path_and_query().ok_or_else(||io_other_error("Invalid URI"))?;
     let mut need_to_insert_host = r.uri().host().is_some();
-    if r.headers().contains_key(http::header::HOST) {
+    if r.headers().contains_key(HOST) {
         need_to_insert_host = false;
     }
     macro_rules! w {
